3db153d640473d52daa68f91e2e5bfbabaf0650d
Omar Shahbaz Khan <ok2610@gmail.com>
2021-09-14 20:53:00 +0200

    Upload scripts to create tree hiearchy. Uses too much hardcoded values, refactoring using parameters is warranted.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # wordnet-hierarchy
 Scripts to get the WordNet hierarchy
+
+## py/makeWordNetDef.py
+
+This file is used to get the WordNet definitions from the 12988 features.
+It requires the class descriptions file along with a file containing the distinct features to get definitions from.
+
+## py/makeHierarchy.py
+
+This uses the output file from makeWordNetDef.py to get the hierarchy path to each word
+
+## py/makeTree.py
+
+This file creates a tree structure using the output file from makeHierarchy.py and the distinct feature file. It can take a file to remove specific nodes from the tree, and a number that determines how many levels from the root to cull, default being none.
---
+++ py/makeTree.py
@@ -0,0 +1,101 @@
+import json
+import argparse
+
+class Node(object):
+    def __init__(self):
+        self.parent = None # Single object
+        self.children = []  # Array of objects
+        self.name = ""
+        self.data = -1
+
+    def add_node(self, name):
+        for child in self.children:
+            if child.name == name:
+                return child
+        new_child = Node()
+        new_child.name = name
+        new_child.parent = self
+        self.children.append(new_child)
+        return new_child
+
+    def is_leaf(self):
+        return not self.data == -1
+
+    def __str__(self):
+        if self.is_leaf() and len(self.children) == 0:
+            p = '"name":"' + str(self.name) + '","id":' + str(self.data)
+            #return p
+        elif self.is_leaf() and len(self.children) > 0:
+            #pp = '"' + str(self.name) + '":' + str(self.data) + '},{' 
+            #p = '"{name}":[{children}]'.format(name=self.name, data=self.data, children=', '.join(map(str, self.children)))
+            p = '"name":"{name}","id":{data},"children":[{children}]'.format(name=self.name, data=self.data, children=', '.join(map(str, self.children)))
+        else:
+            p = '"name":"{name}","id":{data},"children":[{children}]'.format(name=self.name, data=self.data, children=', '.join(map(str, self.children)))
+        return '{' + p + '}'
+
+    def cull(self,drop,exc):
+        if ((len(self.children) <= drop and len(self.children) > 0) or self.name in exc) and not self.is_leaf() and self.parent is not None:
+            for idx,ch in enumerate(self.parent.children):
+                if ch.name == self.name:
+                    self.parent.children[idx] = self.children[0]
+                    self.children[0].parent = self.parent
+                    self.parent.children[idx].cull(drop,exc)
+                    break
+            for i in range(1,len(self.children)):
+                self.parent.children.insert(i,self.children[i])
+                self.children[i].parent = self.parent
+                self.children[i].cull(drop,exc)
+        elif len(self.children) > drop or self.parent is None:
+            for ch in self.children:
+                ch.cull(drop,exc)
+        elif len(self.children) <= drop and len(self.children) > 0  and self.is_leaf():
+            for ch in self.children:
+                ch.cull(drop,exc)
+        for ch1 in self.children:
+            cnt = 0
+            for ch2 in self.children:
+                if ch1.name == ch2.name:
+                    cnt += 1
+            if cnt > 1:
+                self.children.remove(ch1)
+
+HELP_HIEARCHY = 'WordNet Hiearchy File'
+HELP_ID_F = 'Distinct feature id file'
+HELP_EXCLUDE = 'File containing words to remove. 1 word per line'
+HELP_CULL = 'The amount of levels to cull from root'
+parser = argparse.ArgumentParser()
+parser.add_argument('--hierarchy_f', type=str, default='lsc_wn_hierarchy_t6.json')
+parser.add_argument('--id_f', type=str, default='distinct_features_t6.json')
+parser.add_argument('--exclude_f', type=str, default='')
+parser.add_argument('--cull', type=int, default=-1)
+args = parser.parse_args()
+
+data = []
+
+with open(args.hierarchy_f,'r') as f:
+    data = json.load(f)
+
+with open(args.id_f,'r') as f:
+    ids = json.load(f)
+
+exclude = set()
+if args.exclude_f != '':
+    with open(args.exclude_f,'r') as f:
+        exclude = set([s.strip() for s in f.readlines()])
+
+
+root = Node()
+
+for idx,paths in enumerate(data):
+    for path in paths:
+        ch = root
+        for p in path:
+            ch = ch.add_node(p)
+        ch.data = ids[idx]
+
+
+root.name = "root"
+root.data = -1
+if args.cull != -1:
+    root.cull(args.cull,exclude)
+print(root)
